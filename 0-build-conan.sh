7786568f5023b4cd378c330d75c6284838a5f210
Tamas Kenez <tamas.kenez@consultant.LEGO.com>
2024-12-02 22:24:15 +0100

    wip

--- 0-build-conan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-rm -rf id
-conan install conanfile.txt -b missing -pr:b default -of id/cmake -s build_type=Debug
-conan install conanfile.txt -b missing -pr:b default -of id/cmake -s build_type=Release
